b2c3a24527f04b5fad2807e2358ad663c4798ef0
Chyld Medford <chyld.medford@gmail.com>
2017-05-07 16:50:07 -0700

    lint

--- branch-sync/branch-sync.go
+++ branch-sync/branch-sync.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "bufio"
-  "strings"
-  "regexp"
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
 )
 
 func main() {
-  reader := bufio.NewReader(os.Stdin)
-  text, _ := reader.ReadString('\n')
-  text = strings.TrimSpace(text)
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	text = strings.TrimSpace(text)
 
-  re := regexp.MustCompile("(ahead) ([0-9]+)|(behind) ([0-9]+)")
-  matches := re.FindAllStringSubmatch(text, -1)
-  output := ""
+	re := regexp.MustCompile("(ahead) ([0-9]+)|(behind) ([0-9]+)")
+	matches := re.FindAllStringSubmatch(text, -1)
+	var output string
 
-  if len(matches) >= 1{
-    if matches[0][1] == "ahead" {
-      output += "+" + matches[0][2]
-    }
-    if matches[0][3] == "behind" {
-      output += "-" + matches[0][4]
-    }
-  }
-  if len(matches) >= 2{
-    if matches[1][3] == "behind" {
-      output += "/-" + matches[1][4]
-    }
-  }
-  if len(matches) >= 1{
-    output += " "
-  }
-  fmt.Printf("%s", output)
+	if len(matches) >= 1 {
+		if matches[0][1] == "ahead" {
+			output += "+" + matches[0][2]
+		}
+		if matches[0][3] == "behind" {
+			output += "-" + matches[0][4]
+		}
+	}
+
+	if len(matches) == 2 {
+		output += "/-" + matches[1][4]
+	}
+
+	if len(output) > 0 {
+		output += " "
+	}
+
+	fmt.Printf("%s", output)
 }
-
