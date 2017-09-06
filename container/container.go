664695e118bd4a459c6a21fcec4eb7936b517ea4
Chyld Medford <chyld.medford@gmail.com>
2017-09-06 07:50:54 -0700

    container

---
+++ container/container.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	// https://github.com/lizrice/containers-from-scratch
+	// https://github.com/lizrice/strace-from-scratch
+
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("what???")
+	}
+}
+
+func run() {
+	fmt.Printf("running: %v\n", os.Args[2:])
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
